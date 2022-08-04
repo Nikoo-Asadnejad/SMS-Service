931a5cfe1eb0e47086a156123a91f3ed098b89e7
= <91413713+Nikoo-Asadnejad@users.noreply.github.com>
2022-08-04 19:30:45 +0430

    Fix : fix sms service issues

--- SmsService/SmsService/Program.cs
+++ SmsService/SmsService/Program.cs
@@ -1,5 +1,8 @@
 using SmsService.Configurations;
-
+using SmsService.Dtos.Sms;
+using SmsService.Dtos.User;
+using SmsService.Interfaces;
+using SmsService.Percistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,16 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 Configurator.ConfigPipeLines(app);
+
+var smsServce = app.Services.GetService<ISmsService>();
+
+SmsInputDto input = new SmsInputDto(BaseData.SmsTypes.OPT.Id , "123", 
+  BaseData.Providers.Najva.Id,
+  new UserDto(1, "nikoo", "asad", "09393701422"));
+
+smsServce.CreateSmsAsync(input);
+
+
 
 
 
